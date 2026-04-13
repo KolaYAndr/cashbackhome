e270937050be972a97ce7c1bf277f2f81f0f1614
Dias_133 <arysdias133@gmail.com>
2026-04-13 14:43:18 +0300

    Unused imports

--- composeApp/src/commonMain/kotlin/org/homesharing/cashbackhome/presentation/cards/UpsertCardScreen.kt
+++ composeApp/src/commonMain/kotlin/org/homesharing/cashbackhome/presentation/cards/UpsertCardScreen.kt
@@ -27,7 +27,6 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -38,7 +37,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
